065101b1a38b97dc9f49d986fd0894629bb6db88
sangTran <sangthien2010@gmail.com>
2020-04-28 15:21:02 -0500

    typescript types

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello There!");
+const myName: string = "Sang";
+
+const ten: number = 10;
+const negative: number = -100;
+const decimal: number = 10.03;
+
+const yes: boolean = true;
+
+const nothing: null = null;
+const noDefinition: undefined = undefined;
+
+const any: any = "any Type";
